254710f1f0a00c3c161f50e7dabdd078a0bdf966
Semicolon-GD <maerken@icloud.com>
2023-11-15 17:06:53 +0300

    signals

---
+++ Assets/Scripts/Signals/InputSignals.cs
@@ -0,0 +1,35 @@
+using System;
+using Keys;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class InputSignals : MonoBehaviour
+    {
+        
+        #region Singleton
+        
+        public static InputSignals Instance;
+
+        private void Awake()
+        {
+            if (Instance !=null && Instance !=this)
+            {
+                Destroy(gameObject); 
+                return;
+            }
+
+            Instance = this;
+        }
+
+        #endregion
+        
+        
+        public UnityAction onFirstTimeTouchTaken= delegate {  };
+        public UnityAction onInputTaken= delegate {  };
+        public UnityAction onInputRelease= delegate {  };
+        public UnityAction<HorizontalInputParams> onInputDragged= delegate {  };
+        
+    }
+}
